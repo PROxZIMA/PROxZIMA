a6a5f7dbb33ab89d34a5312d6c120b3faba1e2a9
Pratik Pingale <pratikbpingale9075@gmail.com>
2021-10-23 18:58:35 +0530

    Update name.c

--- src/script/name.c
+++ src/script/name.c
@@ -1,1 +1,1 @@
-main(){char*i="+b7BFB,RSXVQ+BCfAQ+b7BN*:?A:;=*::::9;A*::::9;99*::K9:9:+",k=1,j,l=!k,m;do for(m=*i-48,j=l?m/k:m%k;m>>7?k=1<<m+8,!l&&puts(&l)**&l:j--;printf("  \0_/"+l));while((l^=3)||l[++i]);}
+main(){char*i="+b7BFB,RSXVQ+BCfAQ+b7BN*:?A:;=*::::9;A*::::9;99*::K9:9:++",k=1,j,l=!k,m;do for(m=*i-48,j=l?m/k:m%k;m>>7?k=1<<m+8,!l&&puts(&l)**&l:j--;printf("  \0_/"+l));while((l^=3)||l[++i]);}
